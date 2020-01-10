efb78b7d87a92ec1712c84e796fa3a632ec01445
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-01-10 07:03:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
-require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
   def get_page
   doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  
+end
   def get_courses
     self.get_page.css(".post")
   end
