0647f060d4f9f1bba695f72d6d10bebe574ab522
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-01-10 05:46:17 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,7 +2,7 @@ class Course
   @@all = []
   attr_accessor :title, :schedule, :description
   
-  def initialize()
+  def initialize(title, schedule, description)
     @@all << self
   end
   def self.all
